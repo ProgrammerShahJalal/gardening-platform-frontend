5072a28b42016a92d6b0a589ce9b4d672e669366
ProgrammerShahJalal <you@example.com>
2024-10-02 08:38:22 +0600

    project structure

---
+++ src/app/(WithCommonLayout)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <h1 className="text-3xl font-bold">
+        Welcome to{" "}
+        <span className="text-blue-500">Gardening Tips and Advice</span>
+      </h1>
+    </section>
+  );
+}
